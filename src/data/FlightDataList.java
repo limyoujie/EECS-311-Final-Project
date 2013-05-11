f39b7cc98b997267f4b48a3dfefe1c4086515d93
Benedict Lim <benedict@u.northwestern.edu>
2013-05-11 02:21:14 -0500

    EdgeData class created and tested

--- src/data/FlightDataList.java
+++ src/data/FlightDataList.java
@@ -7,8 +7,13 @@ public class FlightDataList {
 	public FlightDataList(){
 		sortedList=new ArrayList<FlightDataNode>();
 	}
-	public void add(FlightDataNode newNode){
+	public FlightDataList(FlightDataNode newNode){
+		sortedList=new ArrayList<FlightDataNode>();
 		sortedList.add(newNode);
+	}
+	public void addNode(FlightDataNode newNode){
+		sortedList.add(newNode);
+		// Everytime a new flight is added, we sort according to price
 		Collections.sort(sortedList);
 	}
 	public void print(){
@@ -16,13 +21,13 @@ public class FlightDataList {
 			sortedList.get(i).print();
 		}
 	}
-	public static void main (String args []){
-		FlightDataNode test[] = {new FlightDataNode(2343,1235,3456,4564),new FlightDataNode(123,1237565,34345356,625),new FlightDataNode(697,123445,3456,8),new FlightDataNode(224,2345,667,4568874),new FlightDataNode(989,4564,3456,1231)};
-		FlightDataList testlist=new FlightDataList();
-		for (int i =0; i<5;i++){
-			testlist.add(test[i]);
-			testlist.print();
-			System.out.println("-----------------------");
-		}
-	}
+//	public static void main (String args []){
+//		FlightDataNode test[] = {new FlightDataNode(2343,1235,3456,4564),new FlightDataNode(123,1237565,34345356,625),new FlightDataNode(697,123445,3456,8),new FlightDataNode(224,2345,667,4568874),new FlightDataNode(989,4564,3456,1231)};
+//		FlightDataList testlist=new FlightDataList();
+//		for (int i =0; i<5;i++){
+//			testlist.addNode(test[i]);
+//			testlist.print();
+//			System.out.println("-----------------------");
+//		}
+//	}
 }
